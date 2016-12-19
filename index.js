5aa128cf843f3e6d00965b402032dcdba58976cb
Jessica Cooper <Jmcooper@jmcooperart.com>
2016-12-19 18:10:58 +0000

    done checking on all inputs

--- index.js
+++ index.js
@@ -98,12 +98,34 @@ document.getElementById('submit').onclick = function(){
   	var calstoreachgoal = 1500 - (((weight - goalweight) * 3500) / daystofinishdate);
 
 	var idealweightrange = 'Your current weight is ' + weight + ' lbs. Your ideal weight range is between ' + Math.round( idealweightlow * 10) / 10 + ' lbs and ' + Math.round( idealweighthigh * 10) / 10 + ' lbs.<br>';
-	var BMIresults = 'Your current BMI is ' + Math.round( BMI * 10) / 10 + '. To achieve your goal BMI of ' + goalBMI + ' you would need to weigh ' + Math.round( goalBMIresult * 10) / 10 + ' lbs.<br>';
+	
+	var BMIresults = 'Your current BMI is ' + Math.round( BMI * 10) / 10 + '.<br>';
+	
 	var BMRresults =  'Your BMR is ' + Math.round( BMR ) + ' calories per day.<br>';
+	
 	var TDEEresults = 'Your TDEE is ' + Math.round( TDEE ) + ' calories per day.<br>';
+	
 	var tolose = 'To lose 0.5 lbs per week, consume ' + Math.round( lose1 ) + ' calories per day.<br> To lose 1 lb per week, consume ' + Math.round( lose2 ) + ' calories per day.<br>To lose 2 lbs per week, consume ' + Math.round( lose3 ) + ' calories per day.<br>';
 	
-	var predictor = 'If you consume ' + intendedcals + ' calories per day, you will reach your goal weight of ' + goalweight + ' lbs on ' + dategoalreached.toDateString() + '.<br>On ' + finishdate.toDateString() + ' you would weigh ' + Math.round(finishdateweight * 10) / 10 + ' lbs.<br>To weigh ' + goalweight + ' lbs on ' + finishdate.toDateString() + ' you would need to consume ' + Math.round(calstoreachgoal) + ' calories per day.<br>';
+	//optionals 
+
+	//goalBMI
+	var goalBMIresults;
+	if (goalBMI === '') {
+		goalBMIresults = '';
+	}
+	else {
+		goalBMIresults = 'To achieve your goal BMI of ' + goalBMI + ' you would need to weigh ' + Math.round( goalBMIresult * 10) / 10 + ' lbs.<br>';
+	}
+
+	//predictor
+	var predictor;
+	if (intendedcals === '' || goalweight === '' || finishdate === '') {
+		predictor = '';
+	}
+	else {
+			predictor = 'If you consume ' + intendedcals + ' calories per day, you will reach your goal weight of ' + goalweight + ' lbs on ' + dategoalreached.toDateString() + '.<br>On ' + finishdate.toDateString() + ' you would weigh ' + Math.round(finishdateweight * 10) / 10 + ' lbs.<br>To weigh ' + goalweight + ' lbs on ' + finishdate.toDateString() + ' you would need to consume ' + Math.round(calstoreachgoal) + ' calories per day.<br>';
+	}
 	
 	var zigzag = 'Zig Zag Diet Planner<br>Day 1 calories: ' + intendedcals * 1.4 + '<br>Day 2 calories: ' + intendedcals * 0.5 + '<br>Day 3 calories: ' + intendedcals * 1.2 + '<br>Day 4 calories: ' + intendedcals * 0.9 + '<br>Day 5 calories: ' + intendedcals * 1.3 + '<br>Day 6 calories: ' + intendedcals * 0.7 + '<br>Day 7 calories: ' + intendedcals + '<br>';
 
@@ -111,7 +133,7 @@ document.getElementById('submit').onclick = function(){
 
 
 
-	var results = 'height is:' + height + '<br><br>' + idealweightrange + BMIresults + BMRresults + TDEEresults + tolose + predictor + zigzag + fivetwo;
+	var results = idealweightrange + BMIresults + goalBMIresults + BMRresults + TDEEresults + tolose + predictor + zigzag + fivetwo;
 
 
 	document.getElementById('results').innerHTML = results;
